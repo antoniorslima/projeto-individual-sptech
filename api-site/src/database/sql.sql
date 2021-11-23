44e697edfc7fae1d0dbd6754d29e8ff6ef9363a5
antoniorslima <antonio.lima@bandtec.com.br>
2021-11-23 13:54:52 -0200

    editaNDO..

--- api-site/src/database/sql.sql
+++ api-site/src/database/sql.sql
@@ -1,4 +1,3 @@
-/* para workbench - local - desenvolvimento */
 CREATE DATABASE ayuda;
 
 USE ayuda;
@@ -10,6 +9,15 @@ CREATE TABLE usuario (
 	senha VARCHAR(50)
 );
 
+insert into usuario (nome, email,senha)
+values
+('Pedro Souza','pedro@gmail.com','pedro123'),
+('Maria Paula','maria@gmail.com','maria123'),
+('Carlos Jose','carlos@gmail.com','carlos123'),
+('Paulo Silva','paulo@gmail.com','paulo123');
+
+select * from usuario;
+
 CREATE TABLE comentario (
 	id INT PRIMARY KEY AUTO_INCREMENT,
     serie VARCHAR(45),
@@ -18,5 +26,17 @@ CREATE TABLE comentario (
 	fk_usuario INT,
 	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
 ); 
+
+insert into comentario (serie, nota, descricao, fk_usuario)
+values
+('Modern Family', 10, 'Melhor série eveeeer, todas as 11 temproadas são demais!',3),
+('Black Mirror', 10, 'Black Mirror é o tipo de serie que voce se arrepender te der demorado tanto tempo pra assistir',4),
+('Pretty Smart', 7, 'Série bem levinha, ideal pra quem quer assistir um besteirol',3),
+('Modern Family', 9, 'Com muitas situações engraçadas e boa química, Modern Family continua sendo uma ótima série, divertida e familiar. Adorei.',2),
+('Modern Family', 10, 'Vida cotidiana de uma família americana moderna, com suas situações diversas. Bem engraçada.',1),
+('Bridgerton', 8, 'Otima série de época, me supreendi com o final.',3),
+('Friends', 10, 'Me lembra os velhos tempos, nao canso de rever',3),
+('One day at a Time', 9, 'Faz muitas críticas sociais fortíssimas, com enredos bem trabalhados e muita empatia.',3);
+
 select * from comentario;
 
